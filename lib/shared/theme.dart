cccfee34b2d5175429c1d1c997cfc6a241b6febd
nurmuh <nurmuhamadrum@gmail.com>
2025-05-22 22:22:02 +0700

    first commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xff5C40CC);
+Color keyBlackColor = const Color(0xff1F1449);
+Color keyWhiteColor = const Color(0xffFFFFFF);
+Color keyGreyColor = const Color(0xff9698A9);
+Color keyGreenColor = const Color(0xff0EC3AE);
+Color keyRedColor = const Color(0xffEB70A5);
+Color keyBackgroundColor = const Color(0xffFAFAFA);
+Color keyInActiveColor = const Color(0xffDBD7EC);
+
+TextStyle blackTextStyle = TextStyle(color: keyBlackColor);
+TextStyle whiteTextStyle = TextStyle(color: keyWhiteColor);
+TextStyle greyTextStyle = TextStyle(color: keyGreyColor);
+TextStyle greenTextStyle = TextStyle(color: keyGreenColor);
+TextStyle redTextStyle = TextStyle(color: keyRedColor);
+TextStyle purpleTextStyle = TextStyle(color: primaryColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
